6c59514721f0ed98bd0b0fb0a8937583963372bf
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-19 12:25:45 +0100

    Add map initialization code

---
+++ main.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([38.912753, -77.032194], 15);
+var gl = L.mapboxGL({
+    accessToken: 'no-token',
+    style: 'https://tile.mapservice.xyz/styles/midnight/style.json',
+}).addTo(map);
