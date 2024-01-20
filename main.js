480c0278a47ec1123598f67f6fb53d71c3aa1b27
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-20 23:01:34 +0100

    Update map zoom settings

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var map = L.map('map').setView([38.912753, -77.032194], 15);
+var map = L.map('map', {
+    minZoom: 9,
+    maxZoom: 14 
+}).setView([38.912753, -77.032194], 15);
+
 var gl = L.mapboxGL({
     style: 'https://tile.mapservice.xyz/styles/midnight/style.json',
 }).addTo(map);
