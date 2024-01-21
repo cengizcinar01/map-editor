a63c3c63403cd1fa51f5da43e6b6dc7a735fba8a
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-22 00:16:09 +0100

    Add Navigation Event Listeners
    These listeners trigger the 'showContent' function, which toggles the display of different content based on the clicked element.

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 var map = L.map('map', {
     minZoom: 9,
-    maxZoom: 14 
+    maxZoom: 14,
 }).setView([38.912753, -77.032194], 15);
 
 var gl = L.mapboxGL({
     style: 'https://tile.mapservice.xyz/styles/midnight/style.json',
 }).addTo(map);
+
+function showContent(contentId) {
+    var contents = ['place', 'style', 'text', 'size'];
+
+    contents.forEach(function (id) {
+        var displayStyle = id === contentId ? 'block' : 'none';
+        document.getElementById(id).style.display = displayStyle;
+    });
+}
+
+document.getElementById('navPlace').addEventListener('click', function () {
+    showContent('place');
+});
+document.getElementById('navStyle').addEventListener('click', function () {
+    showContent('style');
+});
+document.getElementById('navText').addEventListener('click', function () {
+    showContent('text');
+});
+document.getElementById('navSize').addEventListener('click', function () {
+    showContent('size');
+});
