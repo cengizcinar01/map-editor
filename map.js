06df9b6ed33d9a6707cf8b17981221fca5ef7299
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-23 14:59:09 +0100

    Refactor map.js for readability
    This included changes like using const instead of var, using arrow functions, and destruct.

--- map.js
+++ map.js
@@ -1,37 +1,29 @@
-var map = L.map('map', {
-    minZoom: 9,
-    maxZoom: 14,
-}).setView([38.912753, -77.032194], 15);
+const map = L.map('map', { minZoom: 9, maxZoom: 14 }).setView([38.912753, -77.032194], 15);
 
-var gl = L.mapboxGL({
-    style: 'https://tile.mapservice.xyz/styles/midnight/style.json',
-}).addTo(map);
+L.mapboxGL({ style: 'https://tile.mapservice.xyz/styles/midnight/style.json' }).addTo(map);
 
-document.getElementById('searchPlace').addEventListener('input', function (e) {
-    var searchQuery = e.target.value;
-    searchLocation(searchQuery);
-});
+document.getElementById('searchPlace').addEventListener('input', (e) => searchLocation(e.target.value));
 
-function searchLocation(query) {
-    fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(query)}`)
-        .then((response) => response.json())
-        .then((data) => {
-            updateSearchResults(data.features);
-        })
-        .catch((err) => console.error(err));
-}
+const searchLocation = async (query) => {
+    try {
+        const response = await fetch(`https://photon.komoot.io/api/?q=${encodeURIComponent(query)}`);
+        const data = await response.json();
+        updateSearchResults(data.features);
+    } catch (err) {
+        console.error(err);
+    }
+};
 
-function updateSearchResults(results) {
-    var resultsContainer = document.getElementById('searchResults');
+const updateSearchResults = (results) => {
+    const resultsContainer = document.getElementById('searchResults');
     resultsContainer.innerHTML = '';
 
-    results.slice(0, 10).forEach((result) => {
-        var resultItem = document.createElement('div');
-        resultItem.classList.add('search-result-item');
-        resultItem.textContent = result.properties.name;
-        resultItem.onclick = function () {
-            map.setView([result.geometry.coordinates[1], result.geometry.coordinates[0]], 15);
-        };
+    results.slice(0, 10).forEach(({ properties: { name }, geometry: { coordinates } }) => {
+        const resultItem = document.createElement('div');
+        resultItem.className = 'search-result-item';
+        resultItem.textContent = name;
+        resultItem.onclick = () => map.setView([coordinates[1], coordinates[0]], 15);
         resultsContainer.appendChild(resultItem);
     });
-}
+};
+    
